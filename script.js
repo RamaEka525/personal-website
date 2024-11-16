c2c8ae29353067f5d0db445086afec035b224a33
RamaEka525 <ramaekarm@gmail.com>
2024-11-15 23:09:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const boxes = document.querySelectorAll(".box");
     const burgerIcon = document.querySelector(".burger-icon");
     const menuList = document.querySelector(".menu-list");
+    const navLinks = document.querySelectorAll(".nav-link");
     const observerOptions = {
         threshold: 0.1
     };
+    
     burgerIcon.addEventListener("click", () => {
         menuList.classList.toggle("active");
+    });
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            menuList.classList.remove("active");
+        });
     });
     
     function animateOnScroll(entries, observer) {
